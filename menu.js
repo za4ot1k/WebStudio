c8b7b3fc2e324bfb54282f303c485cabffc50a81
za4ot1k <128631886+za4ot1k@users.noreply.github.com>
2024-01-27 10:56:03 +0200

    fix bugs adaptive

--- menu.js
+++ menu.js
@@ -1,15 +1,15 @@
-(() => {
-    const refs = {
-      openMenuBtn: document.querySelector("[data-menu-open]"),
-      closeMenuBtn: document.querySelector("[data-menu-close]"),
-      Menu: document.querySelector("[data-menu]"),
-    };
+// (() => {
+//     const refs = {
+//       openMenuBtn: document.querySelector("[data-menu-open]"),
+//       closeMenuBtn: document.querySelector("[data-menu-close]"),
+//       Menu: document.querySelector("[data-menu]"),
+//     };
   
-    refs.openMenuBtn.addEventListener("click", toggleMenu);
-    refs.closeMenuBtn.addEventListener("click", toggleMenu);
+//     refs.openMenuBtn.addEventListener("click", toggleMenu);
+//     refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
-    function toggleMenu() {
-      refs.Menu.classList.toggle("is-hidden");
-    //   document.body.classList.toggle("no-scroll");
-    }
-})();
+//     function toggleMenu() {
+//       refs.Menu.classList.toggle("is-hidden");
+//     //   document.body.classList.toggle("no-scroll");
+//     }
+// })();
